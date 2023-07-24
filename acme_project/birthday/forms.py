700f86fdf1d7fda3dc4e5fa219e29d021530602d
Илья Бородин <Ilya.borodin.1986@yandex.ru>
2023-07-24 13:36:29 +0300

    RFERGG

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Birthday
 from .validators import real_age
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
@@ -24,11 +25,18 @@ class BirthdayForm(forms.ModelForm):
 
     def clean(self):
         super().clean()
-        # Получаем имя и фамилию из очищенных полей формы.
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
-        # Проверяем вхождение сочетания имени и фамилии во множество имён.
         if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
             )
